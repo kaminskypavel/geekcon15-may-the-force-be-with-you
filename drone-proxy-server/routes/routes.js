bf5485511d7ef99a68debb8197a3d38794be7d55
roeybr <roey.brecher@gmail.com>
2015-09-10 16:25:03 +0300

    =move at all directions

--- drone-proxy-server/routes/routes.js
+++ drone-proxy-server/routes/routes.js
@@ -5,5 +5,7 @@ var droneController = require('../controllers/drone_controller')
 module.exports = function routes() {
   router.post('/takeoff/', droneController.takeOff)
   router.post('/land/', droneController.land)
+  router.post('/stop/', droneController.stop)
+  router.post('/move/:direction', droneController.move)
   return router.routes()
 }
