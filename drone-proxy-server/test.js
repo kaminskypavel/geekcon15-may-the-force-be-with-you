c20925ef49e2fbc6dcd363cbad518a3c640eb597
roeybr <roey.brecher@gmail.com>
2015-09-10 21:30:10 +0300

    tests

--- drone-proxy-server/test.js
+++ drone-proxy-server/test.js
@@ -1,17 +1,20 @@
 var url = 'http://127.0.0.1:6969/';
 var request = require('request');
 
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  while (new Date().getTime() < start + milliseconds);
-}
-
 request.post(url + 'takeoff/');
-sleep(5000);
-request.post(url + 'move/up/0.2/');
-sleep(3000);
-request.post(url + 'stop/');
-sleep(1000);
-request.post(url + 'animate/wave/5000/');
-sleep(1000);
-request.post(url + 'animate/thetaDance/5000/');
+
+setTimeout(function() {
+  request.post(url + 'move/up/0.1/');
+  setTimeout(function() {
+    request.post(url + 'stop/');
+    setTimeout(function() {
+      request.post(url + 'move/left/0.1/');
+      setTimeout(function() {
+        request.post(url + 'move/right/0.1/');
+        setTimeout(function() {
+          request.post(url + 'land');
+          }, 7000);
+        }, 7000);
+      }, 2000);
+    }, 5000);
+  }, 10000);
